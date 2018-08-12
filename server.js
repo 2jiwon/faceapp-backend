fff281977f1a6039dff7ade6a743a5271b237240
2jiwon <ljwjulian@gmail.com>
2018-08-12 20:40:43 +0900

    Separated register part.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const register = require('./controllers/register');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -41,31 +42,7 @@ app.post('/signin', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  const { name, email, password } = req.body;
-  const hash = bcrypt.hashSync(password);
-  db.transaction(trx => {
-    trx.insert({
-      hash: hash,
-      email: email
-    })
-      .into('login')
-      .returning('email')
-      .then(loginEmail => {
-        return trx('users')
-          .returning('*')
-          .insert({
-            name: name,
-            email: loginEmail[0],
-            joined: new Date()
-          })
-          .then(user => {
-            res.json(user[0])
-          })
-      })
-      .then(trx.commit)
-      .catch(trx.rollback)
-  })
-    .catch(err => res.status(400).json('Unable to register'))
+  register.handleRegister(req, res, db, bcrypt)
 })
 
 app.get('/profile/:id', (req, res) => {
